a360fadb859a3e3a580ff1c93cda898e1519c334
JF532 <jfilipe532@hotmail.com>
2026-03-28 14:47:54 -0300

    Alterações do construtor e variáveis

--- src/lista1/exercicio_2/Main.java
+++ src/lista1/exercicio_2/Main.java
@@ -1,6 +1,9 @@
 public class Main {
     public static void main(String[] args) {
-       Triangulo triangulo = new Triangulo(2, 2, 2);
+       Triangulo triangulo = new Triangulo();
+       triangulo.lado1=2;
+       triangulo.lado2=3;
+       triangulo.lado3=4;
 
        triangulo.tipoTriangulo();
     }
