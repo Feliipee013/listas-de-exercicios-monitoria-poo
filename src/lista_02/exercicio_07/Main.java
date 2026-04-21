744c8f12a950b5e3b9194fce04a73afbd87cb222
Feliipee013 <ftaveira611@gmail.com>
2026-04-21 13:36:51 -0300

    Exercício 7 da lista 2 finalizado

---
+++ src/lista_02/exercicio_07/Main.java
@@ -0,0 +1,8 @@
+package lista_02.exercicio_07;
+
+public class Main {
+    public static void main(String[] args) {
+        Desenhista desenhista = new Desenhista();
+        desenhista.desenharQuadrado(7);
+    }
+}
