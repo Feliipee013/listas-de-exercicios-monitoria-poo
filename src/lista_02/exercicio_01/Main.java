8b6709b95c9de1db9e68fa87d496080d057fc9a0
Feliipee013 <ftaveira611@gmail.com>
2026-04-21 12:50:53 -0300

    Exercício 1 da lista 2 finalizado

---
+++ src/lista_02/exercicio_01/Main.java
@@ -0,0 +1,9 @@
+package lista_02.exercicio_01;
+
+public class Main {
+    public static void main(String[] args) {
+        ContadorRegressivo contadorRegressivo = new ContadorRegressivo();
+
+        contadorRegressivo.iniciarContagem(60);
+    }
+}
