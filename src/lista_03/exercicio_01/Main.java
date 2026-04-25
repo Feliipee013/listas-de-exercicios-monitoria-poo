5584cc67b09c4a26173c669f09d55b459e96c318
Feliipee013 <ftaveira611@gmail.com>
2026-04-25 17:12:31 -0300

    Exercício 1 da lista 3 finalizado

---
+++ src/lista_03/exercicio_01/Main.java
@@ -0,0 +1,8 @@
+package lista_03.exercicio_01;
+
+public class Main {
+    public static void main(String[] args) {
+        VetorBasico vetorBasico = new VetorBasico();
+        vetorBasico.imprimirVetor();
+    }
+}
